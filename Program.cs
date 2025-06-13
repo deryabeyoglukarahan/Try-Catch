12b510133ddaf90ace111c0b5b854dae91e7a65a
deryabeyoglukarahan <derya.beyoglu1@gmail.com>
2025-06-13 18:15:38 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Bir sayı giriniz:");
+try
+{
+    int sayi = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Girilen sayının karesi: " + sayi * sayi);
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Geçersiz giriş! Lütfen bir sayı giriniz.");
+    Console.WriteLine(ex.Message);
+}
